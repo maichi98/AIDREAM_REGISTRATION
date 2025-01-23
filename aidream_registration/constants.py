beb0dbfa936f186fc5faac97d5092aa74f2883c6
maichi98 <elaichi.mohammed.1998@gmail.com>
2025-01-23 13:00:05 +0100

    stuff
    # Conflicts:
    #	aidream_registration/labels_registration/register labels on pre_rt T1.ipynb

--- aidream_registration/constants.py
+++ aidream_registration/constants.py
@@ -5,6 +5,7 @@ DIR_DEFAULT_HARD_DRIVE = Path("D:")
 LIST_MRI_MAPS = ["T1", "T1CE", "FLAIR"]
 LIST_CERCARE_MAPS = ["CTH", "rCBV", "OEF", "rCMRO2", "Delay", "rLeakage", "COV", "brainmask"]
 LIST_PERFUSION_TIMEPOINTS = ["first", "last", "avg"]
+
 LIST_INTERPOLATORS = ["linear", "bSpline", "lanczosWindowedSinc", "nearestNeighbor", "genericLabel"]
 
 DIR_DATA = Path(__file__).parent.parent / "data"
